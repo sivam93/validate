a93188a8c2654ff2ee40698784e948d10f907e3f
Sathish Kumar P <SathishKumar@teamblr.com>
2019-09-22 20:14:55 +0530

    ssss

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/ValiateForm'));
 
 app.get('/*', function(req,res) {
     
